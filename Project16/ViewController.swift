560300762367de41b0c083194577292f809d7f78
igibliss00 <igibliss00@gmail.com>
2020-07-01 14:45:26 -0400

    Add map type changing method.

--- Project16/ViewController.swift
+++ Project16/ViewController.swift
@@ -7,12 +7,20 @@
 //
 import MapKit
 import UIKit
+import WebKit
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, WKNavigationDelegate {
 
     @IBOutlet var mapView: MKMapView!
+    var webView: WKWebView!
+    
+//    override func loadView() {
+//        webView = WKWebView()
+//        webView.navigationDelegate = self
+//        view = webView
+//    }
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
         
         let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olympics")
         let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago")
@@ -21,12 +29,29 @@ class ViewController: UIViewController, MKMapViewDelegate {
         let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.8895111, longitude: -77.036667), info: "Named after George himself.")
         
         mapView.addAnnotations([london, oslo, paris, rome, washington])
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(changeMapType))
+    }
+    
+    @objc func changeMapType() {
+        let ac = UIAlertController(title: "Change Map Type", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Standard", style: .default, handler: { [weak self] (_) in
+            self?.mapView.mapType = .standard
+        }))
+        ac.addAction(UIAlertAction(title: "Satellite", style: .default, handler: { [weak self] (_) in
+            self?.mapView.mapType = .satellite
+        }))
+        ac.addAction(UIAlertAction(title: "Hybrid", style: .default, handler: { [weak self] (_) in
+            self?.mapView.mapType = .hybrid
+        }))
+        
+        present(ac, animated: true)
     }
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         guard annotation is Capital else { return nil }
         let identifier = "Capital"
-        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
 
         if annotationView == nil {
             annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
@@ -34,6 +59,7 @@ class ViewController: UIViewController, MKMapViewDelegate {
             
             let btn = UIButton(type: .detailDisclosure)
             annotationView?.rightCalloutAccessoryView = btn
+            annotationView?.pinTintColor = .green
         } else {
             annotationView?.annotation = annotation
         }
@@ -43,12 +69,20 @@ class ViewController: UIViewController, MKMapViewDelegate {
     
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
         guard let capital = view.annotation as? Capital else { return }
-        let placeName = capital.title
-        let placeInfo = capital.info
+//        let placeInfo = capital.info
 
-        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        present(ac, animated: true)
+//        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
+//        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+//        present(ac, animated: true)
+        if let placeName = capital.title {
+            if let url = URL(string: "http://en.wikipedia.org/wiki/" + placeName) {
+                webView.load(URLRequest(url: url))
+            } else {
+                print("website loading failed")
+            }
+        } else {
+            print("place name not found")
+        }
     }
     
 }
